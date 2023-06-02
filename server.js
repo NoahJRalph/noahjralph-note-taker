6808a77b887b7afe33011dba9e79fe32feab111f
Noah Ralph <NoahJRalph@gmail.com>
2023-06-01 21:22:18 -0700

    before heroku deployment

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 // Necessary Declarations
-const express = require('express');
-const path = require('path');
+const express = require(`express`);
+const path = require(`path`);
 const fs = require(`fs`);
-const noteData = require('./db/db.json');
-const PORT = 3001;
+const noteData = require(`./db/db.json`);
+const PORT = process.env.PORT || 3001;
 const app = express();
 // Express Usage
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(`public`));
 // Get Requests
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, `public`, 'index.html'));
+app.get(`*`, (req, res) => {
+  res.sendFile(path.join(__dirname, `/public/index.html`));
 });
 app.get(`/notes`, (req, res) => {
-  res.sendFile(path.join(__dirname, `public`, `notes.html`));
+  res.sendFile(path.join(__dirname, `/public/notes.html`));
 });
-app.get('/api/notes', async (req, res) => res.json(JSON.parse(fs.readFileSync('./db/db.json', 'utf8'))));
+app.get(`/api/notes`, async (req, res) => res.json(JSON.parse(fs.readFile(noteData, `utf8`))));
 // Post Requests
 app.post(`/api/notes`, async (req, res) => {
   // Make regex ID
